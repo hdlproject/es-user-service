e304e4f9f90be0bae3bea21fd14d18926df0a355
HDL Project <hdl.project.co@gmail.com>
2023-06-05 14:44:13 +0700

    test: update password manager test

--- helper/password_test.go
+++ helper/password_test.go
@@ -7,19 +7,38 @@ import (
 )
 
 func TestPasswordManager_IsPasswordValid(t *testing.T) {
-	tests := struct {
-		name string
-		isValid
-	}{}
-
 	passwordManager := NewPasswordManager(uuid.NewString())
 
 	password := uuid.NewString()
 
 	hashedPassword := passwordManager.GetHashedPassword(password)
 
-	isValid := passwordManager.IsPasswordValid(password, hashedPassword)
-	if !isValid {
-		t.Fatalf("invalid password")
+	tests := []struct {
+		name           string
+		password       string
+		hashedPassword string
+		isValid        bool
+	}{
+		{
+			name:           "valid",
+			password:       password,
+			hashedPassword: hashedPassword,
+			isValid:        true,
+		},
+		{
+			name:           "invalid",
+			password:       uuid.NewString(),
+			hashedPassword: hashedPassword,
+			isValid:        false,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			isValid := passwordManager.IsPasswordValid(test.password, test.hashedPassword)
+			if isValid != test.isValid {
+				t.Fatalf("expect is_valid %v but got %v", test.isValid, isValid)
+			}
+		})
 	}
 }
